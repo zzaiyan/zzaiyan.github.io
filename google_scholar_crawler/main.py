1da882d68302da9b5004ddff39a1546452a6be47
zzaiyan <zzaiyan@outlook.com>
2025-06-10 00:05:15 +0800

    add echo

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -3,12 +3,13 @@ import jsonpickle
 import json
 from datetime import datetime
 import os
-# os.environ['GOOGLE_SCHOLAR_ID'] = 'ZpxXejIAAAAJ'
+os.environ['GOOGLE_SCHOLAR_ID'] = 'ZpxXejIAAAAJ'
 
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
+author['citedby'] = sum([pub['num_citations'] for pub in author['publications']])
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
@@ -18,7 +19,7 @@ with open(f'results/gs_data.json', 'w') as outfile:
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author.get('citedby', 0)}",
+  "message": f"{author['citedby']}",
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
